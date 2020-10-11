06a1b022d26c382ce2ef53389f19f441125e1fc0
Brandon <bmehlenbacher4@gmail.com>
2020-10-11 12:11:47 -0500

    Finalizing Cmdline and plot features

--- efficiency_cavity.py
+++ efficiency_cavity.py
@@ -61,7 +61,9 @@ def clipping_losses(radius_mirror,mode_radius_value):
     Inputs:
     radius_mirror: radius of the mirror, this is half the diameter of the mirror if you go back to the old fiber spreadsheets, in microns
     mode_radius_value: the size of the mode when it reaches the mirror, calculated from previous mode_radius_mirror, in microns
-    
+
+    Outputs:
+    Clipping losses associate wtih the mirror
     """
     return np.exp(-2*((radius_mirror)**2)/(mode_radius_value**2))
 def finesse_calculations(cavity_length,mirror_diameter,mode_radius_value):
@@ -75,7 +77,9 @@ def finesse_calculations(cavity_length,mirror_diameter,mode_radius_value):
     Finesse: Calculates the finesse of the cavity in terms of losses
     """
     clipping_loss = clipping_losses(mirror_diameter,mode_radius_value)*2
-    total_losses = 160*10**-6 # tbh i was lazy and decided a constant starting finesse value, this includes absorption,scattering, and transmission losses
+    finesse_initial = 30000
+    total_losses = (2*np.pi)/finesse_initial
+    #total_losses = 180*10**-6 # tbh i was lazy and decided a constant starting finesse value, this includes absorption,scattering, and transmission losses
     return (2*np.pi)/(clipping_loss+total_losses)
 
 def cross_sectional_area(mode_radius):
@@ -88,35 +92,35 @@ def cooperativity(finesse,wavevector,spot_size):
     return (24*finesse)/(np.pi*(wavevector**2)*spot_size**2)
 #Defining Command Line arguments
 
-savefig = False #decides whether or not to save the file
+text = False
 parser = argparse.ArgumentParser(description = "Takes in ROC and Mirror Diameter")
-parser.add_argument("-m","--mirror_diameter", help = "Input mirror diameter in microns", required = False,default = 100)
-parser.add_argument("-r","--radius_of_curvature", help = "Input radius of curvature in microns", required = False,default = 1000)
-parser.add_argument("-s","--save",help = "If passed, enables the save, otherwise figure is displayed", required = False,default = "n")
+parser.add_argument("-m","--mirror_diameter", help = "Input mirror diameter in microns", required = False,default = "100")
+parser.add_argument("-r","--radius_of_curvature", help = "Input radius of curvature in microns", required = False,default = "1000")
+parser.add_argument("-s","--save",help = "1st efficiency, 2nd finesse, 3rd cross_section, 4th F/A and 5th cooperativity, y means savefig", required = False,default = "n,n,n,n,n")
 parser.add_argument("-w","--wavelength",help = 'Wavelength of light in cavity in nanometers',required = False, default = 780)
 parser.add_argument("-c","--core_fiber",help = 'This is the mode field diameter of an optical fiber in microns',required = False,default = 3)
 argument = parser.parse_args()
 
 if argument.mirror_diameter:
-    arg_mir_dia = int(argument.mirror_diameter)
+    arg_mir_dia = list(map(int,argument.mirror_diameter.split(",")))
 if argument.radius_of_curvature:
-    arg_ROC = int(argument.radius_of_curvature)
+    arg_ROC = list(map(int,argument.radius_of_curvature.split(",")))
 if argument.save:
-    if argument.save == "y":
-        savefig = True
-    else:
-        savefig = False
+    savefig = list(argument.save.split(","))
+    if len(savefig) != 5:
+        print("-s requires four arguments for four plots")
+        exit()
 if argument.wavelength:
     wavelength_nm = int(argument.wavelength)
 if argument.core_fiber:
     core_radius = int(argument.core_fiber)
 
-
-mirror_diameter_list = [arg_mir_dia]
-ROC_list = [arg_ROC]
+mirror_diameter_list = arg_mir_dia
+ROC_list = arg_ROC
 wavelength = wavelength_nm*10**-3
 for radius_of_curvature in ROC_list:
     for diameter_mirror in mirror_diameter_list:
+        print(f"Mirror Diameter of {diameter_mirror} and ROC of {radius_of_curvature}")
         radius_mirror = diameter_mirror/2
         max_cavity_length = radius_of_curvature*2
         cavity_length = np.linspace(0.01,max_cavity_length,10000)
@@ -130,41 +134,37 @@ for radius_of_curvature in ROC_list:
         cooperativity_values = cooperativity(finesse,wavevector(wavelength),mode_radius)
         # you can remove the quotes and look at the mode waist as a function of cavity length if you would like but i left this commented out
         #plots the mode waist and the waist at the mirror surface
-        """
-        plt.figure()
-        plt.plot(cavity_length, mode_radius,cavity_length,mode_radius_mirror)
-        #plt.ylim([0,15])
-        plt.show()
-        #plots the efficiency of mode collection as a function of mirror diameter
         
-        plt.figure()
+        #plt.figure()
         plt.plot(cavity_length,efficiency(mode_radius_mirror, core_radius,radius_of_curvature,wavelength),color='black')
-        plt.xlim([000,1500])
+        plt.xlim([000,max_cavity_length])
         plt.title("Coupling Efficiency for Fiber Cavities")
         plt.xlabel(r"Cavity Length ($\mu$m)")
         plt.ylabel("Coupling Efficiency")
-        if savefig:
+        if text:
+            plt.text(x=max_cavity_length/25,y=max(efficiency(mode_radius_mirror, core_radius,radius_of_curvature,wavelength))*.01,s = f"Mirror Diameter of {diameter_mirror} and ROC of {radius_of_curvature}")
+        if savefig[0] == 'y':
             plt.savefig("efficiency_simulations/"+fig_name+"_mirror_diameter_"+str(radius_of_curvature)+"_ROC_efficiency_curve.png")
-            plt.clf()
+            plt.close()
         else:
             plt.show()
-        """
+        
         #plt.figure()
-        
-        
         plt.plot(cavity_length,finesse,color='black')
         plt.xlim([000,max_cavity_length])
         #plt.hlines(5000, 0,max_cavity_length,color = 'red', linestyle='dashed')
         plt.title("Finesse of Fiber Fabry Perot Cavities")
         plt.xlabel(r"Cavity Length ($\mu$m)")
         plt.ylabel("Finesse")
-        if savefig:
+        if text:
+            plt.text(x=max_cavity_length/25,y=max(finesse)*.01,s = f"Mirror Diameter of {diameter_mirror} and ROC of {radius_of_curvature}")
+        if savefig[1] == 'y':
             plt.savefig("F_Plots/"+fig_name+"_mirror_diameter_"+str(radius_of_curvature)+"_ROC_Finesse_curve.png")
-            plt.clf()
+            plt.close()
         else:
             plt.show()
         
-        plt.figure()
+        #plt.figure()
         #finesse = finesse_calculations(cavity_length,radius_mirror,mode_radius_mirror)
         plt.plot(cavity_length,cross_sectional_area_value,color='black')
         plt.xlim([0,max_cavity_length])
@@ -173,13 +173,15 @@ for radius_of_curvature in ROC_list:
         plt.title("Cross Sectional Area of Fiber Fabry Perot Cavities")
         plt.xlabel(r"Cavity Length ($\mu$m)")
         plt.ylabel(r"Cross Sectional Area ($\mu$m$^2$)")
-        if savefig:
+        if text:
+            plt.text(x=max_cavity_length/25,y=6,s = f"Mirror Diameter of {diameter_mirror} and ROC of {radius_of_curvature}")
+        if savefig[2] == 'y':
             plt.savefig("A_Plots/"+fig_name+"_mirror_diameter_"+str(radius_of_curvature)+"_ROC_Cross_Sectional_Area.png")
             plt.close()
         else:
             plt.show()
 
-        plt.figure()
+        #plt.figure()
         plt.plot(cavity_length,finesse/cross_sectional_area_value,color='black')
         plt.xlim([0,max_cavity_length])
         plt.ylim([0,4000])
@@ -187,12 +189,14 @@ for radius_of_curvature in ROC_list:
         plt.title("F/A of Fiber Fabry Perot Cavities")
         plt.xlabel(r"Cavity Length ($\mu$m)")
         plt.ylabel("Finesse/Cross Sectional Area (1/$\mu$m$^2$)")
-        if savefig:
+        if text:
+            plt.text(x=max_cavity_length/25,y=40,s = f"Mirror Diameter of {diameter_mirror} and ROC of {radius_of_curvature}")
+        if savefig[3] == 'y':
             plt.savefig("FA_Plots/"+fig_name+"_mirror_diameter_"+str(radius_of_curvature)+"_ROC_FA_Curve.png")
             plt.close()
         else:
             plt.show()
-        plt.figure()
+        #plt.figure()
         plt.plot(cavity_length,cooperativity_values,color='black')
         plt.xlim([0,max_cavity_length])
         plt.ylim([0,60])
@@ -200,8 +204,10 @@ for radius_of_curvature in ROC_list:
         plt.title("Cooperativity")
         plt.xlabel(r"Cavity Length ($\mu$m)")
         plt.ylabel("Cooperativity")
-        if savefig:
-            #plt.savefig("FA_Plots/"+fig_name+"_mirror_diameter_"+str(radius_of_curvature)+"_ROC_FA_Curve.png")
+        if text:
+            plt.text(x=max_cavity_length/25,y=6,s = f"Mirror Diameter of {diameter_mirror} and ROC of {radius_of_curvature}")
+        if savefig[4] == 'y':
+            plt.savefig("cooperativity/"+fig_name+"_mirror_diameter_"+str(radius_of_curvature)+"_ROC_cooperativity_Curve.png")
             plt.close()
         else:
             plt.show()
